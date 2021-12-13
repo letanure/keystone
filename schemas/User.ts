6dddbc5d2d436010821b8d6928f4b624619f70f2
Luiz Tanure <letanure@gmail.com>
2021-12-13 13:37:32 +0100

    refactor: move schema User

---
+++ schemas/User.ts
@@ -0,0 +1,31 @@
+// We're using some common fields in the starter. Check out https://keystonejs.com/docs/apis/fields#fields-api
+// for the full list of fields.
+import { text, password, relationship } from "@keystone-6/core/fields";
+
+const User = {
+  // Here are the fields that `User` will have. We want an email and password so they can log in
+  // a name so we can refer to them, and a way to connect users to posts.
+  fields: {
+    name: text({ validation: { isRequired: true } }),
+    email: text({
+      validation: { isRequired: true },
+      isIndexed: "unique",
+      isFilterable: true,
+    }),
+    // The password field takes care of hiding details and hashing values
+    password: password({ validation: { isRequired: true } }),
+    // Relationships allow us to reference other lists. In this case,
+    // we want a user to have many posts, and we are saying that the user
+    // should be referencable by the 'author' field of posts.
+    // Make sure you read the docs to understand how they work: https://keystonejs.com/docs/guides/relationships#understanding-relationships
+    posts: relationship({ ref: "Post.author", many: true }),
+  },
+  // Here we can configure the Admin UI. We want to show a user's name and posts in the Admin UI
+  ui: {
+    listView: {
+      initialColumns: ["name", "posts"],
+    },
+  },
+};
+
+export default User;
